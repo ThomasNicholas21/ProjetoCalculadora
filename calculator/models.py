fb8fe085f80113e633043e153a7b428277d4af59
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-07-26 23:32:26 -0300

    feat: criando model para operations

--- calculator/models.py
+++ calculator/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Operation(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='operation',
+        verbose_name='Usuário'
+    )
+    parameters = models.CharField(
+        max_length=128,
+        blank=False,
+        null=False,
+        verbose_name='Expressão'
+    )
+    result = models.CharField(
+        max_length=128,
+        blank=False,
+        null=False,
+        verbose_name='Resultado'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Data de criação'
+    )
